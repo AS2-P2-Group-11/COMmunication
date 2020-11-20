aef0b245990013073d4e19486ede7bbd6d29abf2
mr.salehsedghpour <mr.salehsedghpour@yahoo.com>
2020-11-20 18:39:25 +0100

    Add Item model

--- backend/models.py
+++ backend/models.py
@@ -12,3 +12,11 @@ class Category(Base):
     items = relationship("Item", back_populates="category")
 
 
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), index=True)
+    price = Column(Integer)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="items")
