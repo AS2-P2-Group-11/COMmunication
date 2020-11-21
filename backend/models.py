eef7f62ae02d08965543d14115170d80ce0122e9
mr.salehsedghpour <mr.salehsedghpour@yahoo.com>
2020-11-21 12:21:48 +0100

    Add Order and OrderItem Model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
+
+import datetime
 
 from database import Base
 
@@ -20,3 +22,22 @@ class Item(Base):
     price = Column(Integer)
     category_id = Column(Integer, ForeignKey("categories.id"))
     category = relationship("Category", back_populates="items")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.datetime.now)
+    items = relationship("OrderItem", back_populates="order")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey("items.id"))
+    item = relationship("Item", back_populates="order_items")
+    quantity = Column(Integer)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    order = relationship("Order", back_populates="order_items")
