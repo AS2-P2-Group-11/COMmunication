b0546fdc69be34ca7fa7ca8aaf6294b466e1da5f
mr.salehsedghpour <mr.salehsedghpour@yahoo.com>
2020-11-21 16:22:34 +0100

    get category and category items by name

--- backend/crud.py
+++ backend/crud.py
@@ -7,8 +7,22 @@ def get_category_by_id(db: Session, category_id: int):
     return db.query(models.Category).filter(models.Category.id == category_id).first()
 
 
+def get_category_by_name(db: Session, category_name: str):
+    return db.query(models.Category).filter(models.Category.name == category_name).first()
+
+
 def delete_category_by_id(db: Session, category_id: int):
     obj = db.query(models.Category).filter(models.Category.id == category_id).first()
+    try:
+        db.delete(obj)
+        db.commit()
+    except:
+        return None
+    return obj
+
+
+def delete_category_by_name(db: Session, category_name: str):
+    obj = db.query(models.Category).filter(models.Category.name == category_name).first()
     try:
         db.delete(obj)
         db.commit()
@@ -29,6 +43,11 @@ def create_category(db: Session, category: schemas.CategoryCreate):
     return db_category
 
 
-def get_category_items_by_id(db: Session, category_id: int, skip: int=0, limit: int = 100):
+def get_category_items_by_id(db: Session, category_id: int, skip: int = 0, limit: int = 100):
     return db.query(models.Item).filter(models.Item.category_id == category_id).offset(skip).limit(limit).all()
+
+
+def get_category_items_by_name(db: Session, category_name: str, skip: int = 0, limit: int = 100):
+    category = get_category_by_name(db, category_name=category_name)
+    return db.query(models.Item).filter(models.Item.category_id == category.id).offset(skip).limit(limit).all()
 
--- backend/main.py
+++ backend/main.py
@@ -33,9 +33,25 @@ def read_category_by_id(category_id: int, db: Session = Depends(get_db)):
     return category
 
 
+@app.get("/category_by_name/{category_name}", response_model=schemas.Category)
+def read_category_by_id(category_name: str, db: Session = Depends(get_db)):
+    category = crud.get_category_by_name(db, category_name=category_name)
+    if category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return category
+
+
 @app.delete("/category_by_id/{category_id}", response_model=schemas.Category, status_code=200)
 def remove_category_by_id(category_id: int, db: Session = Depends(get_db)):
     category = crud.delete_category_by_id(db, category_id=category_id)
+    if category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return category
+
+
+@app.delete("/category_by_name/{category_name}", response_model=schemas.Category, status_code=200)
+def remove_category_by_name(category_name: str, db: Session = Depends(get_db)):
+    category = crud.delete_category_by_name(db, category_name=category_name)
     if category is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return category
@@ -46,10 +62,17 @@ def write_category(category: schemas.CategoryCreate, db: Session = Depends(get_d
     return crud.create_category(db=db, category=category)
 
 
-@app.get("/categories/{category_id}/items", response_model=List[schemas.Item])
-def read_category_items(category_id, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@app.get("/category_by_id/{category_id}/items", response_model=List[schemas.Item])
+def read_category_items_by_category_id(category_id, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_category_items_by_id(db, category_id=category_id, skip=skip, limit=limit)
     return items
+
+
+@app.get("/category_by_name/{category_name}/items", response_model=List[schemas.Item])
+def read_category_items_by_category_name(category_name, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_category_items_by_name(db, category_name=category_name, skip=skip, limit=limit)
+    return items
+
 
 
 
