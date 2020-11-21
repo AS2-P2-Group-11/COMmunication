f0dbc7a745f48f725a2ffa823c5fa0252e040c5f
mr.salehsedghpour <mr.salehsedghpour@yahoo.com>
2020-11-21 13:46:48 +0100

    Fix the bug in DB

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .common import ConfigManager
+from common import ConfigManager
 
 config_manager = ConfigManager()
 db = {
