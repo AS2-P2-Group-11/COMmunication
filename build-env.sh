5222c1d5a7e9c5a034a08b526a99899a2aeda796
mr.salehsedghpour <mr.salehsedghpour@yahoo.com>
2020-11-22 14:08:35 +0100

    adding db initialize and update build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 docker build --tag p2_11_api:1.0 .
+docker-compose up -d
+sleep 5
+docker-compose restart api
+sleep 2
+python3 db_initialize.py
---
+++ db_initialize.py
@@ -0,0 +1,47 @@
+import requests
+api = "http://127.0.0.1:9000"
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+products = {
+    "laptop": [
+        {
+            "title": "macbook",
+            "price": 1500
+        },
+        {
+            "title": "lenovo",
+            "price": 1300
+        },
+        {
+            "title": "dell",
+            "price": 1400
+        }
+    ],
+    "mobile": [
+        {
+            "title": "iphone",
+            "price": 800
+        },
+        {
+            "title": "samsung",
+            "price": 700
+        },
+    ],
+    "tablet": [
+        {
+            "title": "ipad",
+            "price": 1100
+        },
+        {
+            "title": "galaxy note",
+            "price": 1000
+        },
+    ]
+}
+
+for category, list_prod in products.items():
+    test = requests.post(api+"/categories/", json={"name": str(category)}, headers=headers )
+    for product in list_prod:
+        requests.post(api+"/category_by_name/"+category+"/items", json=product, headers=headers)
