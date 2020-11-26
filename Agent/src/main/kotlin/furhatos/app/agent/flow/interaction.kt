f91b5b35b41651641d7e1158b1557a4b5554a673
Stefan <stefan.kaalen@hotmail.com>
2020-11-26 13:10:16 +0100

    runable version

--- Agent/src/main/kotlin/furhatos/app/agent/flow/interaction.kt
+++ Agent/src/main/kotlin/furhatos/app/agent/flow/interaction.kt
@@ -1,8 +1,11 @@
-package furhatos.app.communication.flow
+package furhatos.app.agent.flow
 
+import furhatos.app.agent.nlu.*
+import furhatos.flow.kotlin.State
+import furhatos.flow.kotlin.furhat
+import furhatos.flow.kotlin.onResponse
+import furhatos.flow.kotlin.state
 import furhatos.nlu.common.*
-import furhatos.flow.kotlin.*
-import furhatos.app.communication.nlu.*
 
 //Greeting
 val Start = state(Interaction) {
@@ -24,73 +27,81 @@ val ChooseAction = state(Interaction) {
     //What happens on response nothing?
 
     //If the user wants to place an order
-    onResponse<NewOrder> {
+    onResponse<PlaceOrder> {
         //--->Create an empty shopping cart here<---
         goto(ChooseShoppingCartAction)
     }
 
-    onResponse<ListOptions> {
+    onResponse<RequestOptions> {
         furhat.say {"You can place an order, change an order, or track an order"}
         reentry()
     }
 
-    onResponse {
-        furhat.say("I'm sorry, I didn't catch that.")
-        furhat.say("You can place an order, change an order, or track an order")
-        reentry()
+    onResponse<GoodBye>{
+        furhat.say{"So long"}
+        goto(Idle)
     }
+
+    //onResponse {
+    //    furhat.say("I'm sorry, I didn't catch that.")
+    //    furhat.say("You can place an order, change an order, or track an order")
+    //    reentry()
+    //}
 }
 
 
 val ChooseShoppingCartAction = state(Interaction){
     onEntry {
         //Get ShoppingCart.items from API
-        if (ShoppingCart.items.isEmpty()){
+        //if (ShoppingCart.items.isEmpty()){
             furhat.say("Your shopping cart is currently empty")
-        }
-        else{
-            furhat.say {
-                +"Your shopping cart currently consist of"
-                //Get ShoppingCart.items from API
-                +println(ShoppingCart.items) }
-        }
+        //}
+        //else{
+        //    furhat.say {
+        //        +"Your shopping cart currently consist of"
+        //        //Get ShoppingCart.items from API
+        //        +println(ShoppingCart.items) }
+        //}
         furhat.ask("What do you want to do with it?")
     }
 
+    //The user wants to checkout (place) his order
     onResponse<Checkout> {
         //Get ShoppingCart.items from API
-        if(ShoppingCart.items.isEmpty()){
-            furhat.say{"The shopping cart is empty and can not be checked out!"}
-            furhat.say {"Your choices are to add items, or aborting the order"}
-            reentry()
-        }
-        else{
-            furhat.say {
-                +"Your order has been placed and your order number is "
+        //if(ShoppingCart.items.isEmpty()){
+        //    furhat.say{"The shopping cart is empty and can not be checked out!"}
+        //    furhat.say {"Your choices are to add items, or aborting the order"}
+        //    reentry()
+        //}
+        //else{
+        //    furhat.say {
+        //        +"Your order has been placed and your order number is "
                 //Get ShoppingCart.id from API
-                +ShoppingCart.id
-                +"Have a nice day!"
-            }
+        //        +ShoppingCart.id
+        //        +"Have a nice day!"
+        //    }
             goto(Idle)
-        }
+        //}
     }
-    onResponse<AddItems> {
-        goto(ListShoppingItems)
+    onResponse<Add> {
+        goto(ListShoppingCategories)
     }
-    onResponse<ChangeItem> {
-        if (ShoppingCart.items.isEmpty()){
+
+    //Maybe change instead of remove in nlu?
+    onResponse<Remove> {
+        //if (ShoppingCart.items.isEmpty()){
             furhat.say { "Your shopping cart is empty, you can't change an item in it." }
             reentry()
-        }
-        else {
-            goto(ChooseItemToChange)
-        }
+        //}
+        //else {
+        //    goto(ChooseItemToRemove)
+        //}
     }
-    onResponse<AbortOrdering> {
+    onResponse<Aborting> {
         furhat.say("Order aborted")
-        goto(ChooseAction)
+        //goto(ChooseAction)
     }
-    onResponse<ListOptions> {
+    onResponse<RequestOptions> {
         furhat.say {"Your choices are to checkout your order, add items, change items, or aborting the order"}
         reentry()
     }
@@ -101,22 +112,22 @@ val ChooseShoppingCartAction = state(Interaction){
     }
 }
 
-val ListShoppingItems = state(Interaction){
+val ListShoppingCategories = state(Interaction){
     onEntry {
-        furhat.ask {"Do you want me to list the available items in the store?"}
+        furhat.ask {"Do you want me to list the available categories of items in the store?"}
     }
 
+    //Prints out the list "Category" gotten from the nlu
     onResponse<Yes> {
-        furhat.say {
-            +"The available items are"
-            //Get Shop.items from API
-            +Shop.Items
-        }
-        goto(AddShoppingItems)
+    //    furhat.say {
+    //        +"The available categories are"
+    //        +Category
+    //    }
+        goto(ListShoppingItems)
     }
 
     onResponse<No> {
-        goto(AddShoppingItems)
+        goto(ListShoppingItems)
     }
 
     onResponse {
@@ -125,68 +136,97 @@ val ListShoppingItems = state(Interaction){
     }
 }
 
-val AddShoppingItems = state(ChooseShoppingCartAction){
+val ListShoppingItems = state(Interaction){
     onEntry {
-        furhat.ask("What items do you want to add to the list?")
+        furhat.ask {"Do you want me to list the available items in the store?"}
     }
 
-    onResponse<StoreItem> {
-        //Update ShoppingCart in the API
-        val chosenItem = it.intent.itemsInStore
-        goto(SpecifyQuantity(chosenItem))
+    //Prints out the list "Items" gotten from the nlu
+    onResponse<Yes> {
+        furhat.say {
+    //        +"The available items are ${Item().optionsToText}"
+        }
+        goto(AddShoppingCategory)
     }
-    onResponse<ListOptions> {
+
+    onResponse<No> {
+        goto(AddShoppingCategory)
+    }
+
+    onResponse {
+        furhat.say ( "Sorry, I did not catch that" )
+        reentry()
+    }
+}
+
+//Reads in the category the user wants to add an item from
+val AddShoppingCategory: State = state(ChooseShoppingCartAction){
+    onEntry {
+        furhat.ask("From what category do you want to add an item?")
+    }
+
+    onResponse<ChooseCategory> {
+        //Update ShoppingCart in the API
+        val chosenCategory = it.intent.category
+        if (chosenCategory != null){
+            goto(AddShoppingItems(chosenCategory))
+        }
+        else{
+            propagate()
+        }
+    }
+    onResponse<RequestOptions> {
+        furhat.say {"The available categories are ${Category().optionsToText()}"}
         furhat.say {"If you do not want to add more items, your choices are to checkout your order, change items, or aborting the order"}
         reentry()
     }
 }
 
-fun SpecifyQuantity(chosenItem: itemsInCart) : State=state(Interaction){
+//Reads in the item(s) the user want to add to the list (currently from all items but should only be from the chosen category)
+fun AddShoppingItems(category: Category) = state(ChooseShoppingCartAction){
     onEntry {
-        furhat.ask("How many"+chosenItem+" do you want to add to in your shopping cart?")
+        furhat.ask("What items in the category ${category.text} do you want to add to the list?")
     }
-    onResponse<IntegerNumber> {
-        //Get the number from nlu
-        val quantity=it.intent.integerNumber
-        //Change quantity in API
-        ShoppingCart.items.add.quantity=quantity
+
+    onResponse<AddItem> {
+        //Update ShoppingCart in the API
+        //ShoppingCart.items.add.item=item
+        //ShoppingCart.items.add.quantity=quantity
         goto(ChooseShoppingCartAction)
     }
-
-    onResponse {
-        furhat.say { "I did not catch that"}
+    onResponse<RequestOptions> {
+        //furhat.say {"The available items in the category ${category.text} are"+store.items(category)}"}
+        furhat.say {"If you do not want to add more items, your choices are to checkout your order, change items, or aborting the order"}
         reentry()
     }
+    //onResponse<ChooseCategory>{
+    //    val newCategory = it.intent.category
+    //    if (newCategory != null){
+    //        goto(AddShoppingItems(newCategory))
+    //    }
+    //    else{
+    //        propagate()
+    //    }
+    //}
 }
 
-val ChooseItemToChange = state(ChooseShoppingCartAction){
+val ChooseItemToRemove = state(ChooseShoppingCartAction){
     onEntry {
-        furhat.ask("What item do you want to change or remove?")
+        furhat.ask("What item do you want remove?")
     }
-    onResponse<ItemInCart> {
-        val itemToChange = it.intent.itemsInCart
-        goto(SpecifyNewQuantity(itemToChange))
+    onResponse<RemoveItem> {
+       // val itemToRemove = it.intent.itemsInCart
+        //Remove item from order here
+        //        ...
+        //
+        goto(ChooseShoppingCartAction)
     }
-    onResponse<ListOptions> {
+    onResponse<RequestOptions> {
         furhat.say {"If you do not want to change or remove an item, your choices are to checkout your order, add an item, or aborting the order"}
         reentry()
     }
-}
-
-fun SpecifyNewQuantity(itemToChange: itemsInCart) : State=state(Interaction){
-    onEntry {
-        furhat.ask("How many"+itemToChange+"Do you want to keep in your shopping cart?")
-    }
-    onResponse<IntegerNumber> {
-        //Get the number from nlu
-        val quantity=it.intent.integerNumber
-        //Change quantity in API
-        ShoppingCart.items(itemToChange).quantity=quantity
-        goto(ChooseShoppingCartAction)
-    }
-
-    onResponse {
-        furhat.say { "I did not catch that, you currently have "+ShoppingCart.items(itemToChange).quantity+" in your shopping cart" }
+    onResponse{
+        furhat.say{"I'm sorry, I did not catch that"}
         reentry()
     }
 }
--- Agent/src/main/kotlin/furhatos/app/agent/nlu/nlu.kt
+++ Agent/src/main/kotlin/furhatos/app/agent/nlu/nlu.kt
@@ -1,1 +1,300 @@
 package furhatos.app.agent.nlu
+
+import com.google.gson.Gson
+import furhatos.nlu.ComplexEnumEntity
+import furhatos.nlu.EnumEntity
+import furhatos.nlu.Intent
+import furhatos.nlu.ListEntity
+import furhatos.util.Language
+import furhatos.nlu.common.Number
+
+/**
+ * Intent. The user wants to know what options are currently available.
+ *
+ * The response should be context sensitive.
+ */
+class RequestOptions: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("What options do you have?",
+                "What can I do?",
+                "What are the alternatives?",
+                "What are my options?")
+    }
+}
+
+/**
+ * Intent. The system has asked whether something should be added or removed. The user indicates "Add"
+ */
+class Add: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'll add something",
+                "I would like to add to my order",
+                "Add",
+                "I'll add, please")
+    }
+}
+
+/**
+ * Intent. The system has asked whether something should be added or removed. The user indicates "Remove"
+ */
+
+class Remove: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'll remove something",
+                "I would like to remove my order",
+                "Remove",
+                "I want to remove, please")
+    }
+}
+
+data class CategoryData (
+        var name: String,
+        var id: Int? = null,
+        var items: List<ItemData>? = null
+)
+
+data class ItemData(
+        var title: String,
+        var id: Int? = null,
+        var category_id: Int? = null,
+        var price: Int? = null
+)
+
+
+
+
+
+/**
+ * EnumEntity. All available categories.
+ * Currently hardcoded, should be fetched from API.
+ */
+class Category: EnumEntity() {
+    fun getNames(aList: List<CategoryData>): List<String>{
+        return aList.map { it.name }.toList()
+    }
+
+    override fun getEnum(lang: Language): List<String> {
+        val targetURL = "http://127.0.0.1:9000/categories"
+        val response = khttp.get(targetURL).text
+        val categories = Gson().fromJson(response, CategoryList::class.java)
+        return getNames( categories )
+        // return listOf("tablet", "laptop", "mobile")
+    }
+
+}
+
+class CategoryList: ArrayList<CategoryData>()
+
+/**
+ * EnumEntity. All available items.
+ * Currently hardcoded, should be fetched from API.
+ */
+class Item: EnumEntity(stemming = true, speechRecPhrases = true) {
+    fun getNames(aList: List<ItemData>): List<String> {
+        return aList.map { it.title }.toList()
+    }
+
+    fun getItems(aList: List<CategoryData>): List<ItemData>{
+        val returnList = ArrayList<List<ItemData>>()
+        for(data in aList){
+            if (data.items != null){
+                returnList.add(data.items!!)
+            }
+        }
+        return returnList.flatten()
+    }
+
+    override fun getEnum(lang: Language): List<String> {
+        val targetURL = "http://127.0.0.1:9000/categories"
+        val response = khttp.get(targetURL).text
+        val categories = Gson().fromJson(response, CategoryList::class.java)
+        println(getNames(getItems(categories)))
+        return getNames(getItems(categories))
+        // return listOf("Lenovo", "Apple", "Asus")
+    }
+}
+
+/**
+ * EnumEntity. Numbered items, see Fruit Seller example.
+ */
+class QuantifiedItem(
+        val count : Number? = Number(1),
+        val item : Item? = null) : ComplexEnumEntity() {
+
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("@count @item", "@item")
+    }
+
+    override fun toText(): String {
+        return generate("$count $item")
+    }
+}
+
+
+/**
+ * Intent. The user is choosing which category to find item from.
+ */
+class ChooseCategory(var category: Category? = null): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@category",
+                "Something from @category",
+                "What do you have in @category ?")
+    }
+}
+
+/**
+ * Intent. The user wishes to add a specific item to their shopping cart.
+ */
+class AddItem(var item: Item? = null): Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@item",
+                "I'll add an @item",
+                "@item, please.",
+                "Add @item to the shopping cart")
+    }
+}
+
+/**
+ * Intent. The user wishes to remove a specific item from their shopping cart.
+ */
+class RemoveItem(var item: Item): Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I don't need @item",
+                "I'll remove an @item",
+                "Remove the @item from the shopping cart",
+                "I don't need the @item",
+                "I changed my mind about the @item")
+    }
+}
+
+class ChangeItem(var item: Item): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I want to change the @item",
+                "Let's change an @item",
+                "I want to take a look at @item")
+    }
+}
+/**
+ * ListEntity. A lists of numerals constitutes a tracking number.
+ */
+class TrackingNumber: ListEntity<Numeral>()
+
+class ItemsInCart: ListEntity<Item>()
+
+/**
+ * EnumEntity. Could probably be done easier, but this is a way to make numerals concrete.
+ */
+class Numeral(val num: Number? = Number(0)): ComplexEnumEntity(){
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("@num")
+    }
+
+    override fun toText(): String {
+        return generate("$num")
+    }
+}
+
+/**
+ * Intent. The user is inputting a tracking number into the system upon request.
+ */
+class InputTrackingNumber(var trackingNumber: TrackingNumber? = null): Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@trackingNumber",
+                "It's @trackingNumber",
+                "The tracking number is @trackingNumber")
+    }
+}
+
+/**
+ * Intent. The user is checking out their shopping cart.
+ */
+class Checkout: Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'm done shopping",
+                "I'll go to checkout",
+                "Checkout, please.",
+                "I would like to check out my shopping cart")
+    }
+}
+
+/**
+ * Intent. The user is aborting the current operation. The system should return to root state.
+ */
+class Aborting: Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("Never mind",
+                "Abort",
+                "Let's pretend this didn't happen",
+                "Abort my current order")
+    }
+}
+
+/**
+ * Intent. The user indicates that they are leaving the system.
+ */
+class GoodBye: Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("Good bye",
+                "Thanks for all your help",
+                "That will be all",
+                "Goodbye",
+                "I'm leaving")
+    }
+}
+
+/**
+ * Intent. The user wishes to check the contents of their shopping cart.
+ */
+class CheckShoppingCart: Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("What's in my shopping cart?",
+                "Check shopping cart",
+                "What do I have so far?")
+    }
+}
+
+/**
+ * Intent. The user wants to place a new order. Should only be called from root state.
+ */
+class PlaceOrder: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'd like to place an order",
+                "Place an order",
+                "Place")
+    }
+}
+
+/**
+ * Intent. The user wants to cancel an existing order. Should only be called from root state.
+ */
+class CancelOrder: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'd like to cancel my order",
+                "Cancel an order",
+                "Cancel")
+    }
+}
+
+/**
+ * Intent. The user wants to change an existing order. Should only be called from root state.
+ */
+class ChangeOrder: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'd like to change my order",
+                "Modify an order",
+                "Change")
+    }
+}
+
+/**
+ * Intent. The user wants to check the status of an existing order. Should only be called from root state.
+ */
+class CheckStatus: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("What's my order status?",
+                "I'd like to check my order status",
+                "I want to know where my order is",
+                "I want to know where my package is")
+    }
+
+}
