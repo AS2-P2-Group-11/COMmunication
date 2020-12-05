99d5541838238a85fa801fb7d54d2eb586726412
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2020-12-05 17:20:08 +0100

    containerize skill

--- start-agent.sh
+++ start-agent.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-docker build --tag p2_11_api:1.0 .
-docker build --tag p2_11_agent:1.0 ./Agent/
 docker-compose up -d
 sleep 10
 docker-compose restart api
 sleep 10
 docker-compose restart agent
-python3 db_initialize.py
 echo "Agent is ready to work"
